28fe558e6306068899057a4d683aa34218e35d91
Jerry_Bao <jerrybao1998@outlook.com>
2018-07-02 13:10:50 -0600

    Enabled seeding for the DB

--- MvcFlashcards/MvcFlashcards/Models/Flashcard.cs
+++ MvcFlashcards/MvcFlashcards/Models/Flashcard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,8 +12,11 @@ namespace MvcFlashcards.Models
         //ID is a must
         public int ID { get; set; }
         public string Question { get; set; }
+
+        [Display(Name = "Date Added")]
+        [DataType(DataType.Date)]
         public DateTime AddedDate { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
         public string Answer { get; set; }
         public string Topic { get; set; }
     }
---
+++ MvcFlashcards/MvcFlashcards/Models/SeedData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcFlashcards.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcFlashcardsContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MvcFlashcardsContext>>()))
+            {
+                // Look for any flashcards.
+                // If there are any, then no new ones are added
+                if (context.Flashcard.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Flashcard.AddRange(
+                     new Flashcard
+                     {
+                         Question = "Just a seeding test",
+                         AddedDate = DateTime.Parse("1989-1-11"),
+                         Answer = "No answer to this question",
+                         Topic = "Chemistry"
+                     }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
